24abdb9e54b938add8ffd8edc258f22dc719d6be
Cihan <officialcihan0248@gmail.com>
2024-05-10 19:40:20 +0300

    units

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,16 +2,20 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
-  if(!userProgress || !userProgress.activeCourse) {
-    redirect("/courses")
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
   }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -25,6 +29,11 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
